303e805676ec0df190e04885d1a9646f80b2b498
Paul Scott <8pscott@gmail.com>
2020-07-26 00:48:25 -0400

    fixed deployment issues

--- src/app/classes/Ket.ts
+++ src/app/classes/Ket.ts
@@ -1,11 +1,10 @@
 export enum Ket {
-  KET_0 = '<img class="ket" src="../../assets/images/kets/ket-0.svg" alt="|0⟩"/>',
-  KET_1 = '<img class="ket" src="../../assets/images/kets/ket-1.svg" alt="|1⟩"/>',
-  KET_00 = '<img class="ket" src="../../assets/images/kets/ket-00.svg" alt="|00⟩"/>',
-  KET_01 = '<img class="ket" src="../../assets/images/kets/ket-01.svg" alt="|01⟩"/>',
-  KET_10 = '<img class="ket" src="../../assets/images/kets/ket-10.svg" alt="|10⟩"/>',
-  KET_11 = '<img class="ket" src="../../assets/images/kets/ket-11.svg" alt="|11⟩"/>',
-  KET_ALPHA = '<img class="ket" src="../../assets/images/kets/ket-alpha.svg" alt="|α⟩"/>',
-  KET_BETA = '<img class="ket" src="../../assets/images/kets/ket-beta.svg" alt="|β⟩"/>',
-  KET_ALPHA_BETA = '<img class="ket" src="../../assets/images/kets/ket-alpha-beta.svg" alt="|αβ⟩"/>'
+  KET_0 = '<img class="ket" src="assets/images/kets/ket-0.svg" alt="|0⟩"/>',
+  KET_1 = '<img class="ket" src="assets/images/kets/ket-1.svg" alt="|1⟩"/>',
+  KET_00 = '<img class="ket" src="assets/images/kets/ket-00.svg" alt="|00⟩"/>',
+  KET_01 = '<img class="ket" src="assets/images/kets/ket-01.svg" alt="|01⟩"/>',
+  KET_10 = '<img class="ket" src="assets/images/kets/ket-10.svg" alt="|10⟩"/>',
+  KET_11 = '<img class="ket" src="assets/images/kets/ket-11.svg" alt="|11⟩"/>',
+  KET_ALPHA = '<img class="ket" src="assets/images/kets/ket-alpha.svg" alt="|α⟩"/>',
+  KET_ALPHA_BETA = '<img class="ket" src="assets/images/kets/ket-alpha-beta.svg" alt="|αβ⟩"/>'
 }
